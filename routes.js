c2c919266796fe64f3f7c7196966b91b5039686d
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2022-09-08 18:45:54 +0200

    add form with a username input and a POST request to /create-user

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
-
-// route 1 = "/"
-// route 1 = "/users"
+const { text } = require('stream/consumers');
 
 const requestHandler = (req, res) => {
     const url = req.url;
@@ -11,15 +9,18 @@ const requestHandler = (req, res) => {
         res.write('<head><title>Task 1</title></head>');
         res.write('<body>');
         res.write('<h1>Welcome user</h1>');
+        res.write('<form action ="/create-user" method="POST">');
+        res.write('<label>name</label>');
+        res.write('<input type= text name= username />');
+        res.write('<button type="submit">Send</button>');
+        res.write('</form>');
         res.write('</body>');
-        // res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
     if (url === '/users') {
         res.write('<html>');
-
         res.write('<body>');
         res.write('<ul>');
         res.write('<li>User1</li>');
