acdb3a3df36f8ada5ed883800b0beb4122b4627e
JesusIasenzaniro <81029273+JesusIasenzaniro@users.noreply.github.com>
2022-09-08 18:25:27 +0200

    create greeting message in / and a list of users in /users

--- routes.js
+++ routes.js
@@ -1,7 +1,35 @@
 const fs = require('fs');
 
+// route 1 = "/"
+// route 1 = "/users"
+
 const requestHandler = (req, res) => {
-    console.log(req);
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Task 1</title></head>');
+        res.write('<body>');
+        res.write('<h1>Welcome user</h1>');
+        res.write('</body>');
+        // res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/users') {
+        res.write('<html>');
+
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>User1</li>');
+        res.write('<li>User2</li>');
+        res.write('<li>User3</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
 };
 
 module.exports = requestHandler;
